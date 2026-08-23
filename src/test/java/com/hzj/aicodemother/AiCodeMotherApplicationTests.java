fa63c3bee1f11d897afbe525b6900c7055d746e2
“hzj666-cpu” <2398237686@qq.com>
2026-08-23 21:59:31 +0800

    first

---
+++ src/test/java/com/hzj/aicodemother/AiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzj.aicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
