42d3f581e3ef0967c4ed8150658e8e2e26f11960
“hzj666-cpu” <2398237686@qq.com>
2026-08-24 21:31:14 +0800

    后台常用工具，前端创建初始化

---
+++ src/main/java/com/hzj/aicodemother/controller/HealthController.java
@@ -0,0 +1,22 @@
+package com.hzj.aicodemother.controller;
+
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import com.hzj.aicodemother.common.BaseResponse;
+import com.hzj.aicodemother.common.ResultUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@Tag(name = "健康检查类")
+public class HealthController {
+    @GetMapping("/")
+    @Operation(summary = "健康检查")
+    public BaseResponse <String> health(){
+        return ResultUtils.success("ok") ;
+    }
+}
